3cc42549249fc7aadc0c8231cf443f6a8e96e83f
bharath_su <bharath.suryakanthan@photoninfotech.net>
2016-02-02 14:16:47 +0530

    For Front End Development Session 3 Assignment 1 and 2

--- Session5-Assignments/js/hello-world.js
+++
@@ -1,4 +0,0 @@
-function myFunction() {
-    return "Hello World!"; 
-}
-document.getElementById("text1").innerHTML = myFunction(); 
--- Session5-Assignments/js/remainder.js
+++
@@ -1,5 +0,0 @@
-function remainderOperation(x,y) {
-   var z = x % y;
-    return "Remainder = " + z; 
-}
-document.getElementById("remainder").innerHTML = remainderOperation(15,9);
